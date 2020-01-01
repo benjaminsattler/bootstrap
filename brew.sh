6e3321cb18d601ec153a213a087d0de73aceaae6
Benjamin Sattler <bsattler.inbox@gmail.com>
2020-01-01 22:48:47 +0100

    Remove old brew data csv file. Update brew script to use Brewfile

--- brew.sh
+++ brew.sh
@@ -8,7 +8,7 @@ function usage() {
     echo Usage: $0 [-h] [-i \<CSV file\>]
     echo
     echo -h: Print this help information
-    echo -i: Read brew packages from the given csv file instead of the default
+    echo -i: Read brew packages from the given Brewfile instead of the default
     echo
 }
 
@@ -21,31 +21,7 @@ while getopts "i:h" o; do case "${o}" in
 
 esac done
 
-[[ -z "${packagesfile}" ]] && packagesfile=https://raw.githubusercontent.com/benjaminsattler/bootstrap/master/data/brew.csv
+[[ -z "${packagesfile}" ]] && packagesfile=https://raw.githubusercontent.com/benjaminsattler/bootstrap/master/Brewfile
 
-oldifs=${IFS}
-IFS=,
-curl -fsSL "${packagesfile}" | while read package type
-do
-    [[ "${type}" == "cask" ]] && cask=cask || cask=''
-    tmpfile=$(mktemp)
-    exec 3>"${tmpfile}"
-    exec 4<"${tmpfile}"
-    rm ${tmpfile}
-    ${brew} ${cask} list ${package} > /dev/null 2>&1
-    if [[ "$?" == "0" ]]
-    then
-        echo Upgrading ${package}
-        $(${brew} ${cask} upgrade ${package} >&3 2>&1)
-    else
-        echo Installing ${package}
-        ${brew} ${cask} install ${package}
-    fi
-    brewstatus=$?
-    err=$(cat <&4)
-    if [[ "${brewstatus}" != "0" ]]
-    then
-        $(echo ${err} | grep "already installed" > /dev/null 2>&1) || echo ${err}
-    fi
-done
+curl -fsSL "${packagesfile}" | brew bundle --file=-
 
