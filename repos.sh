4b7d9e766a4df36dfae114b7c7c012b07054a3c9
Benjamin Sattler <bsattler.inbox@gmail.com>
2020-01-05 16:11:45 +0100

    Adapt repos.sh script to new data file structure

--- repos.sh
+++ repos.sh
@@ -5,58 +5,46 @@ set +evx
 git=git
 
 function usage() {
-    echo Usage: "$0" [-h] [-r \<CSV file\>] [-t \<directory\>]
+    echo Usage: "$0" [-h] [-r \<CSV file\>]
     echo
     echo -h: Print this help information
     echo -r: Read repositories from the given csv file instead of the default
-    echo -t: Clone repositories into the given directory instead of the default
     echo
 }
 
-while getopts "r:t:h" o; do case "${o}" in
+while getopts "r:h" o; do case "${o}" in
     r) reposfile=${OPTARG} ;;
-    t) repostargetdir=${OPTARG} ;;
     h) usage; exit 0; ;;
     ?) usage; exit 0; ;;
 esac done
 
 [[ -z "${reposfile}" ]] && reposfile=https://raw.githubusercontent.com/benjaminsattler/bootstrap/master/data/repos.csv
-[[ -z "${repostargetdir}" ]] && repostargetdir=~/src/
 
 echo Using repositories from ${reposfile}
-echo Cloning repos into ${repostargetdir}
-
 
 if ! type ${git} > /dev/null 2>&1; then
     echo Git must be available
     exit 1
 fi
 
-if [[ ! -d "${repostargetdir}" ]]; then
-    mkdir -p ${repostargetdir}
-fi
-
 IFS=,
-curl -fsSl "${reposfile}" | while read -r repourl
+curl -fsSl "${reposfile}" | while read -r repourl repotargetdir
 do
     tmpfile=$(mktemp)
     exec 3>"${tmpfile}"
     exec 4<"${tmpfile}"
     rm "${tmpfile}"
     target=$(echo "${repourl}" | rev | cut -d / -f 1 | cut -d . -f '2-' | rev)
-    if [[ -d "${repostargetdir}${target}" ]]
+    repotargetdir=$(eval echo "${repotargetdir}")
+    if [[ -d "${repotargetdir}" ]]
     then
         echo Updating "${target}" to latest version
-        pushd "${repostargetdir}${target}" > /dev/null || exit
-        ${git} pull >&3 2>&1
+        ${git} -C "${repotargetdir}" pull >&3 2>&1
         gitstatus=$?
-        popd > /dev/null || exit
     else
-        echo Cloning "${target}"
-        pushd ${repostargetdir} > /dev/null || exit
-        ${git} clone "${repourl}" >&3 2>&1
+        echo Cloning "${target}" into ${repotargetdir}
+        ${git} clone "${repourl}" "${repotargetdir}" >&3 2>&1
         gitstatus=$?
-        popd > /dev/null || exit
     fi
     [[ "${gitstatus}" != "0" ]] && cat <&4
 done
